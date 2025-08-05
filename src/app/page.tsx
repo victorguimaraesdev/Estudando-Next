559d57191a08476ff6de00991749c9b70a03e958
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-08-05 14:18:29 -0300

    style: stabilization containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
   return (
     <div className="h-screen w-screen bg-black">
       <img src="assets/coffe/coffe1.jpg" alt="Logo" className="w-full h-auto" />
+      <div className="w-full h-200 bg-[#1f1307]">
+      </div>
     </div>
   );
 }
